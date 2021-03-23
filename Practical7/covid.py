d4c2ebc5a708f9ea8bb11431d79cde95e5f07eb4
ZhaoXinyue20 <xinyuez.20@intl.zju.edu.cn>
2021-03-24 01:04:30 +0800

    add covid.py

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -34,7 +34,7 @@ world_new_deaths=covid_data.loc[L,"new_deaths"]
 
 np.mean(world_new_cases)
 np.median(world_new_cases)
-
+# plot world new cases
 score = world_new_cases
 plt.boxplot(score,
              vert = True,
@@ -50,8 +50,23 @@ plt.xlabel('world_dates')
 plt.ylabel('world_new_cases')
 plt.show()
 
-plt.plot(world_dates, world_new_cases, 'b+')
+# plot world new deaths
+score = world_new_deaths
+plt.boxplot(score,
+             vert = True,
+             whis = 1.5,
+             patch_artist = True,
+             meanline = False,
+             showbox = True,
+             showcaps = True,
+             showfliers = True,
+             notch = False
+             )
+plt.xlabel('world_dates')
+plt.ylabel('world_new_deaths')
+plt.show()
 
+#to solve my questions
 Loc=[]
 for i in range (0,7996):
 	if covid_data.iloc[i,1]=="China":
@@ -59,14 +74,13 @@ for i in range (0,7996):
 	else:
 		Loc.append(False)
 
-#my questions
 China_new_cases=covid_data.loc[Loc,"new_cases"]
 China_total_cases=covid_data.loc[Loc,"total_cases"]
 China_dates=covid_data.loc[Loc,"date"]
-plt.plot(China_dates, China_new_cases, 'bo')
+plt.plot(China_dates, China_new_cases, 'b+')
 plt.plot(China_dates, China_total_cases, 'r+')
 plt.xticks(China_dates.iloc[0:len(China_dates):4],rotation=-90)
 plt.xlabel('dates')
-plt.ylabel('population')
-plt.legend()
+plt.ylabel('cases')
 plt.show()
+
