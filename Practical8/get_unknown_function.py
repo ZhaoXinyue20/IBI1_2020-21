59ac8a1ae7aa1664d2d13bf92a0024bda307353e
ZhaoXinyue20 <xinyuez.20@intl.zju.edu.cn>
2021-04-01 19:00:52 +0800

    add get_unknown_function.py

--- Practical8/get_unknown_function.py
+++ Practical8/get_unknown_function.py
@@ -5,43 +5,32 @@ import codecs
 os.chdir("/Users/zhaoxinyue/Documents/GitHub/IBI1_2020-21/Practical8")
 file=open("Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa","r")
 origin = file.readlines()
-# set some variable to temporarily put things
 output = []
-Len=''
-data=""
-
 #find unknow function DNA and extract them
 for i in range(len(origin)):
     if origin[i].startswith('>') and re.search(r'unknown function', origin[i]):               
         name=re.search(r'(>.+?)_',origin[i])
         Name=name.group()
-        output.append(Name)
-        exp = ''            
+        output.append(Name)            
         a = ''
-        exp += '\n'
         for j in range(len(origin[i:-1])):
             if origin[i+j+1].startswith('>'): 
               break
             else:
-              a += origin[i+j+1]
-        exp +=a .replace('\n','')
-        output.append(exp)
-
-# then count the length of the gene
-        Len = ''   
-        output.append(Len)
-
-for i in range(len(output)): 
+              a += origin[i+j+1][:-1]
+        a += "\n"
+        output.append(a)
+# get the length of the gene
+        length = ''
+        output.append(length)
+for i in range(len(output)):
     if output[i].startswith('>'):
-        Len=str(len(output[i+1])-1)
-        output[i]+= '\n' + Len + '\n'
-        
+        length=str(len(output[i+1])-1)
+        output[i] += "  "
+        output[i] += length + "\n"
+
 # save the new file
 fout = codecs.open('unknown_function.fa', "w") 
-for line in output:
-    fout.write(line)
+for i in output:
+    fout.write(i)
 fout.close()
-
-
-                
-
