7c963533cc6eda3c55467d6e4da5e06413cbd3c3
ZhaoXinyue20 <xinyuez.20@intl.zju.edu.cn>
2021-04-09 15:50:56 +0800

    add r_rate.py

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -4,11 +4,11 @@ count = 0
 nterms = 5
 # set r rate by users
 print ("r is")
-r = input()
+r = float(input())
 # calculate
 while count < nterms:
   nth = n1*(r+1)
   n1 = nth
   count += 1
-print('r rate is', str(r) ,'total patients are', str(nth))
+print('r rate is', str(r) ,","+'total patients are', str(nth))
 
