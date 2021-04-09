183eff1f5621f961cd0ea3e1df8cb73295a3deab
ZhaoXinyue20 <xinyuez.20@intl.zju.edu.cn>
2021-04-09 15:41:33 +0800

    add fibonacci.py

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,12 +1,14 @@
-# inital settings
+# set how mant numbers I want to get and the first two numbers 
 nterms = 13
 n1 = 1
 n2 = 1
+# count how many numbers I already have
 count = 2
 while count < nterms:
     nth = n1 + n2
-# reset the number
+# reset the number for the loop
     n1 = n2
     n2 = nth
     count += 1
+# print the result
     print(nth)
