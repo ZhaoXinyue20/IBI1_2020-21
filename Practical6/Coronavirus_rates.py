17bb9aff5717c702d9b7abd4b65ff31ceb913540
ZhaoXinyue20 <79906365+ZhaoXinyue20@users.noreply.github.com>
2021-04-09 16:04:36 +0800

    Update Coronavirus_rates.py

--- Practical6/Coronavirus_rates.py
+++ Practical6/Coronavirus_rates.py
@@ -7,9 +7,9 @@ labels = 'USA', 'Inddia', 'Brazil', 'Russia', 'UK', 'Others'
 sizes = [29862124, 11285561, 11205972, 4360823, 4234924, 59959870]
 explode = (0,0,0,0,0,0)
 colors = ['red', 'yellow', 'green', 'lightskyblue', 'yellowgreen', 'pink']
-# text1 outside, text2 inside, autopct数值保留固定小数位，shadowF无阴影设置，startangle逆时针起始角度设置，pctistance数值距圆心半径倍数距离
+# text1 outside, text2 inside, autopct:Values are kept in fixed decimal places，shadowF:No shadow setting，startangle:Set counterclockwise starting Angle，pctistance: distance multiple from the center of the circle
 pathes,text1,text2 = plt.pie(sizes, explode=explode, labels=labels,colors=colors, autopct='%1.1f%%', shadow=False, startangle=90, pctdistance = 0.6)
-# x,y轴刻度设置一致保证饼图为圆形
+# x,y axis:The scale is set uniformly to ensure that the pie is round
 plt.axis('equal')
 plt.title('Covid-19 patients in the world')
 plt.show()
