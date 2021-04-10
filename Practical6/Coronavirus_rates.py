8eb6a4def86ea6df463f90f994672c538bdce1ca
ZhaoXinyue20 <xinyuez.20@intl.zju.edu.cn>
2021-04-10 10:11:06 +0800

    add Coronavirus_rates.py

--- Practical6/Coronavirus_rates.py
+++ Practical6/Coronavirus_rates.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 # make freqeucy directionary
 My_dict={}
 Countries = {'USA':29862124, 'India':11285561, 'Brazil':11205972, 'Russia':4360823, 'UK':4234924}
+print (Countries)
 # making pie chart
 labels = 'USA', 'India', 'Brazil', 'Russia', 'UK'
 sizes = [29862124, 11285561, 11205972, 4360823, 4234924]
