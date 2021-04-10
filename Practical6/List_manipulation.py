2cb52fa45af8cae2035e7a2f49487b8124d725ea
ZhaoXinyue20 <79906365+ZhaoXinyue20@users.noreply.github.com>
2021-04-10 09:37:56 +0800

    Update List_manipulation.py
    add sorting function, and average exon lengths

--- Practical6/List_manipulation.py
+++ Practical6/List_manipulation.py
@@ -1,10 +1,27 @@
-# sorting lists
+import matplotlib.pyplot as plt
+import numpy as np
+# set two lists
 gene_lengths=[9410,394141,4442,105338,19149,76779,126550,36296,842,15981]
 exon_counts=[51,1142,42,216,25,650,32533,57,1,523]
-# sorted values for the average exon length across 10 genes
-gene_lengths.sort()
-# make boxplot
+gene_lengths=np.array(gene_lengths)
+exon_counts=np.array(exon_counts)
+average_length=gene_lengths/exon_counts
+# change array to list
+average_lengths=list(average_length)
+# sort lengths
+average=sorted(average_lengths)
+print ("the average lengths are:" + str(average))
+# make a boxplot
 n = 10
-score = gene_lengths
-plt.boxplot(score, vert=True,whis=1.5,patch_artist=True, meanline=False, showbox=True, showcaps=True, notch=False)
+score = average_lengths
+plt.boxplot(score,
+            vert=True, #boxplot is vertically presented
+            whis=1.5, #specifies how far the upper and lower quartiles must be from the upper and lower quartiles. 
+            patch_artist=True, #the boxplot has color
+            meanline=False, #the mean as a line
+            showbox=True, #presented as a box
+            showcaps=True, #two lines at the top and end
+            notch=False #no notches
+            )
+plt.title('the distribution of the average exon length')
 plt.show()
