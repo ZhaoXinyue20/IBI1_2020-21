2d5ef2201294f6f1203aa305e589125a432228a7
ZhaoXinyue20 <79906365+ZhaoXinyue20@users.noreply.github.com>
2021-05-16 10:20:04 +0800

    Update List_manipulation.py

--- Practical6/List_manipulation.py
+++ Practical6/List_manipulation.py
@@ -5,6 +5,7 @@ gene_lengths=[9410,394141,4442,105338,19149,76779,126550,36296,842,15981]
 exon_counts=[51,1142,42,216,25,650,32533,57,1,523]
 gene_lengths=np.array(gene_lengths)
 exon_counts=np.array(exon_counts)
+# calculate the average length by dividing one list to the other
 average_length=gene_lengths/exon_counts
 # change array to list
 average_lengths=list(average_length)
