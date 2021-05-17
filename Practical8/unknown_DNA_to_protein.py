0a9d9e29e7fae11c43e4f9ba486a1cc312c32e4a
ZhaoXinyue20 <xinyuez.20@intl.zju.edu.cn>
2021-05-17 19:29:48 +0800

    add unknown_DNA_to_protein.py

--- Practical8/unknown_DNA_to_protein.py
+++ Practical8/unknown_DNA_to_protein.py
@@ -2,28 +2,29 @@ import re
 import codecs
 import os
 # read fasta file as a list
-infile = input("enter your file address here:")
-file=open(infile,"r")
+os.chdir("/Users/zhaoxinyue/Documents/GitHub/IBI1_2020-21/Practical8")
+file=open("unknown_function.fa","r")
 origin = file.readlines()
 protein=[]
 seq=[]
 #translation
-genetic_code = {"TTT":"F","TTC":"F","TTA":"L","TTG":"L",
-                    "CTT":"L","CTC":"L","CTA":"L","CTG":"L",
-                    "ATT":"I","ATC":"I","ATA":"I","ATG":"M",
-                    "GTT":"V","GTC":"V","GTA":"V","GTG":"V",
-                    "TCT":"S","TCC":"S","TCA":"S","TCG":"S",
-                    "CCT":"P","CCC":"P","CCA":"P","CCG":"P",
-                    "ACT":"T","ACC":"T","ACA":"T","ACG":"T",
-                    "GCT":"A","GCC":"A","GCA":"A","GCG":"A",
-                    "TAT":"Y","TAC":"Y","TAA":"STOP","TAG":"STOP",
-                    "CAT":"H","CAC":"H","CAA":"Q","CAG":"Q",
-                    "AAT":"N","AAC":"N","AAA":"K","AAG":"K",
-                    "GAT":"D","GAC":"D","GAA":"E","GAG":"E",
-                    "TGT":"C","TGC":"C","TGA":"STOP","TGG":"W",
-                    "CGT":"R","CGC":"R","CGA":"R","CGG":"R",
-                    "AGT":"S","AGC":"S","AGA":"R","AGG":"R",
-                    "GGT":"G","GGC":"G","GGA":"G","GGG":"G"}
+genetic_code = {'TTT':'F', 'TTC':'F', 'TTA':'L', 'TTG':'L',
+'TCT':'S', 'TCC':'S', 'TCA':'S', 'TCG':'S',
+'TAT':'Y', 'TAC':'Y', 'TAA':'O', 'TAG':'U',
+'TGT':'C', 'TGC':'C', 'TGA':'X', 'TGG':'W',
+'CTT':'L', 'CTC':'L', 'CTA':'L', 'CTG':'L',
+'CCT':'P', 'CCC':'P', 'CCA':'P', 'CCG':'P',
+'CAT':'H', 'CAC':'H', 'CAA':'Q', 'CAG':'Z',
+'CGT':'R', 'CGC':'R', 'CGA':'R', 'CGG':'R',
+'ATT':'I', 'ATC':'I', 'ATA':'J', 'ATG':'M',
+'ACT':'T', 'ACC':'T', 'ACA':'T', 'ACG':'T',
+'AAT':'N', 'AAC':'B', 'AAA':'K', 'AAG':'K',
+'AGT':'S', 'AGC':'S', 'AGA':'R', 'AGG':'R',
+'GTT':'V', 'GTC':'V', 'GTA':'V', 'GTG':'V',
+'GCT':'A', 'GCC':'A', 'GCA':'A', 'GCG':'A',
+'GAT':'D', 'GAC':'D', 'GAA':'E', 'GAG':'E',
+'GGT':'G', 'GGC':'G', 'GGA':'G', 'GGG':'G'}
+
 for i in range (len(origin)):
 	if origin[i].startswith('>'):
 		seq.append(origin[i].split(' ')[0])
@@ -46,4 +47,3 @@ for i in range(len(seq)):
     fout.write('\n')
     
 fout.close()
-
